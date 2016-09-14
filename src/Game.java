3a09bb5935155ae3cdec8091f18cde126c714c98
jtumms <john.tumminelli@tummsmedia.com>
2016-09-14 12:41:54 -0400

    added while loop

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * Created by john.tumminelli on 9/14/16.
  */
 public class Game {
-    static Scanner scanner = new Scanner(System.in); //moved scnanner method to static
+    static Scanner scanner = new Scanner(System.in); //moved scanner method to static
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome, Traveller!");
