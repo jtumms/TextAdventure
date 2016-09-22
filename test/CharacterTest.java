20861edd4955221d12728b6c56620608e13f370e
jtumms <john.tumminelli@tummsmedia.com>
2016-09-22 10:46:23 -0400

    Created battleTest

---
+++ test/CharacterTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by john.tumminelli on 9/22/16.
+ */
+public class CharacterTest {
+    @Test
+    public void testBattle() {
+        Player player = new Player();
+        player.name = "Test Player";
+        player.location = "tunnel";
+        player.weapon = "sword";
+        Enemy enemy =  new Enemy("Test Enemy", 10, 10);
+        player.battle(enemy);
+        assertTrue(player.health > 0);
+        assertTrue(enemy.health < 0);
+    }
+
+}
